0f7a5a0f0125b09e1eb06b61dc2f244d8077733a
Your Name <you@example.com>
2023-05-22 21:15:33 +0300

    Replacing values

--- SOLID_I/src/App.java
+++ SOLID_I/src/App.java
@@ -5,14 +5,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         Circle circle1 = new Circle(5);
-        Cube cube1 = new Cube(10);
+        Cube cube1 = new Cube(5);
 
         double circleArea = circle1.area();
         double cubeArea = cube1.area();
         double cubeVolume = cube1.volume();
 
         System.out.println("Площадь круга радиусом 5: " + circleArea);
-        System.out.println("Площадь куба со стороной 10: " + cubeArea);
-        System.out.println("Объем куба со стороной 10: " + cubeVolume);
+        System.out.println("Площадь куба со стороной 5: " + cubeArea);
+        System.out.println("Объем куба со стороной 5: " + cubeVolume);
     }
 }
